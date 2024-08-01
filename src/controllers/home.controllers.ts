659c11ea2d41c6d833a34087c6b7cc4a93e136f0
Lợi <loivantran10012002@gmail.com>
2024-08-02 00:28:51 +0700

    create stats page

--- src/controllers/home.controllers.ts
+++ src/controllers/home.controllers.ts
@@ -34,3 +34,5 @@ export const dashboardPage = (req: Request, res: Response) => {
     child: '../dashboard/add-job.ejs'
   })
 }
+
+export const getStatsPage = (req: Request, res: Response) => {}
--- src/routes/home.routes.ts
+++ src/routes/home.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import PATH from '~/constants/path.constants'
-import { dashboardPage, landingPage } from '~/controllers/home.controllers'
+import { dashboardPage, getStatsPage, landingPage } from '~/controllers/home.controllers'
 const homeRouter = Router()
 
 homeRouter.route(PATH.LANDING).get(landingPage)
+homeRouter.route(PATH.STATS).get(getStatsPage)
 homeRouter.route(PATH.DEFAULT_PATH).get(dashboardPage)
 
 export default homeRouter
