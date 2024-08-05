745647df26df1c42eb89954958db15fce38815bd
Lợi <loivantran10012002@gmail.com>
2024-08-05 22:40:34 +0700

    fix mesage

--- src/routes/home.routes.ts
+++ src/routes/home.routes.ts
@@ -1,18 +1,10 @@
 import { Router } from 'express'
 import PATH from '~/constants/path.constants'
-<<<<<<< HEAD
-import { dashboardPage, getEditJobPage, landingPage } from '~/controllers/home.controllers'
+import { createJob, dashboardPage, getEditJobPage, landingPage } from '~/controllers/home.controllers'
 const homeRouter = Router()
 
 homeRouter.route(PATH.LANDING).get(landingPage)
 homeRouter.route(PATH.EDIT_JOB).get(getEditJobPage)
-homeRouter.route(PATH.DEFAULT_PATH).get(dashboardPage)
-=======
-import { createJob, dashboardPage, landingPage } from '~/controllers/home.controllers'
-const homeRouter = Router()
-
-homeRouter.route(PATH.LANDING).get(landingPage)
 homeRouter.route(PATH.DEFAULT_PATH).get(dashboardPage).post(createJob)
->>>>>>> 899736cc98e94a3dcf7d50470ee0b7b5ee950c35
 
 export default homeRouter
